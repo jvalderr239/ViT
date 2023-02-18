f8414b7f71704a41dcecd20ab1d34806585ce1d9
Jose Valderrama <jvalderr@gatech.edu>
2023-02-17 20:46:52 -0700

    feature: conv and linear handling for patch embedding

--- src/models/patch_embeddings.py
+++ src/models/patch_embeddings.py
@@ -1,11 +1,12 @@
 from functools import partial
 
 import torch.nn.functional as F
-from torch import Tensor, nn
+from torch import Tensor, nn, reshape
 
 
 class LambdaPatchLayer(nn.Module):
-    def __init__(self, kernel: int = 16):
+    
+    def __init__(self, method="linear", kernel: int = 16):
         """
         Custom layer to generate image patches
 
@@ -14,7 +15,10 @@ class LambdaPatchLayer(nn.Module):
         """
         super(LambdaPatchLayer, self).__init__()
         self.kernel = kernel
-
+        self.mapping = {
+            "linear": self.patch,
+            "conv": 1 ### update
+        }
     def forward(self, x: Tensor) -> Tensor:
         return self.patch(x)
 
@@ -45,8 +49,7 @@ class PatchEmbedding(nn.Module):
 
     Source: https://arxiv.org/pdf/2010.11929.pdf
     """
-
-    def __init__(self, channels: int = 3, patch_size: int = 16, emb_size: int = 768):
+    def __init__(self, channels: int = 3, patch_size: int = 16, emb_size: int = 768, method: str = "conv"):
         """
         Generate linear projection layer to map image patches to token embedding layer
 
@@ -54,15 +57,23 @@ class PatchEmbedding(nn.Module):
             channels -- Number of channels for image
             patch_size -- Dimension of square image patch
             emb_size -- size of token embeddings
+            method -- method to use for image patching
         """
         super().__init__()
-        self.projection = nn.Sequential(
-            ## authors use conv layer for performance gain that uses kernel size of patch
-            ## apply convolution to each patch and then flatten
-            ## nn.Conv2d(channels, emb_size, kernel_size=patch_size, stride=patch_size),
-            LambdaPatchLayer(kernel=patch_size),
-            nn.Linear(patch_size * patch_size * channels, emb_size)
-        )
+        self.method = method
+        if self.method == "conv":
+            self.projection = nn.Sequential(
+                ## authors use conv layer for performance gain that uses kernel size of patch
+                ## apply convolution to each patch and then flatten
+                nn.Conv2d(channels, emb_size, kernel_size=patch_size, stride=patch_size),
+                )
+        elif self.method == "linear":
+            self.projection = nn.Sequential(
+                LambdaPatchLayer(kernel=patch_size),
+                nn.Linear(patch_size * patch_size * channels, emb_size)
+            )
+        else:
+            raise ValueError(f"Unrecognized patching method: {method}")
 
     def forward(self, img: Tensor) -> Tensor:
         """
@@ -74,4 +85,9 @@ class PatchEmbedding(nn.Module):
         Returns:
             1D linear projection of image patches
         """
-        return self.projection(img)
+        if self.method == "linear":
+            return self.projection(img)
+        b, *_ = img.shape
+        conv_output = self.projection(img)
+        _ , c2, h2, w2 = conv_output.shape
+        return conv_output.reshape(b, h2 * w2, c2)
